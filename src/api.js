902f80146ddffa36378f6b2d93606045b1c9bd4d
bonganimasemola <bonganimasemola@icloud.com>
2023-11-07 16:38:16 +0300

    components set up

--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ const API = 'https://restcountries.com/v3.1/all';
 
 async function fetchCountriesData() {
   try {
+    
     const response = await fetch(API);
     if (response.ok) {
       const data = await response.json();
