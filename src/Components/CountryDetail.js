5c84bacda9e3f1f5e9106c835dafa9f9bc4c929c
bonganimasemola <bonganimasemola@icloud.com>
2023-11-08 14:23:28 +0300

    CountryDetail component initial set up

---
+++ src/Components/CountryDetail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function CountryDetail({ countries }) {
+  const { id } = useParams();
+
+  const country = countries.find(country => country.name.common === id);
+
+  if (!country) {
+    return <div>Country not found</div>;
+  }
+
+  return (
+    <div>
+      <h2>Country Details</h2>
+      <h3>{country.name.common}</h3>
+      <p>Capital: {country.capital || 'N/A'}</p>
+    </div>
+  );
+}
+
+export default CountryDetail;
