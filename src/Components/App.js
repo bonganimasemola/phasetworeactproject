8e0a48b9ac35175b1d6a3f63e5343afdbfcfcaf0
bonganimasemola <bonganimasemola@icloud.com>
2023-11-08 14:10:45 +0300

    initial set up of App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,2 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { fetchCountriesData } from '../api'; 
+
+import Header from './Header';
+import CountryList from './CountryList';
+import CountryDetail from './CountryDetail';
+import CountrySearch from './CountrySearch';
+
+function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetchCountriesData()
+      .then(data => {
+        setCountries(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<CountryList countries={countries} />} />
+        <Route path="/country/:id" element={<CountryDetail countries={countries} />} />
+        <Route path="/search" element={<CountrySearch countries={countries} />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
 
 
